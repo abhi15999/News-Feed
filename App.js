55bf6cabb56f518c8362985f461a17cc956ec496
Abhishek Gupta <abhishekgupta1599@gmail.com>
2020-11-27 00:46:53 +0530

    Setup Ready!

---
+++ App.js
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import {
+  View,
+  Text,
+
+} from 'react-native';
+
+
+const App =()=> {
+  return (
+    <>
+      <View>
+        <Text> Hi</Text>
+      </View>
+    </>
+  );
+};
+
+
+
+export default App;
